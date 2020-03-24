08c00fe43de8c153af1daff03d3562c5e76f71dd
Pace Willisson <pace@alum.mit.edu>
2020-03-23 22:19:09 -0400

    wtest

--- wtest.py
+++ wtest.py
@@ -1,11 +1,38 @@
 #!/usr/bin/env python3
 
 import gattlib
+import struct
+import posix
+import sys
+
+if posix.getuid() != 0:
+    print("must be root")
+    sys.exit(1)
 
 req = gattlib.GATTRequester("DC:99:65:B8:F1:F6", False)
 req.connect(True, "random")
 val = req.read_by_uuid("00002a00-0000-1000-8000-00805f9b34fb")
 print(val)
 
-req.write_cmd(0x13, bytes([0xa]))
+def add_cksum(pkt):
+    cksum = ~sum(pkt) & 0xff
+    return pkt + bytes((cksum,))
+
+def mkpkt(button, pressed):
+    fmt = '<2sss'
+    hdr = b'!B'
+    
+    button_byte = button.encode('utf8')
+    if pressed:
+        pressed_byte = b'1'
+    else:
+        pressed_byte = b'0'
+
+    pkt = struct.pack(fmt, hdr, button_byte, pressed_byte)
+    pkt = add_cksum(pkt)
+    print(pkt)
+    return(pkt)
+
+
+req.write_cmd(0x13, mkpkt('1', True))
 print("write done")
