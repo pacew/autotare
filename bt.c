e6f21880f421aa70c4105aa8270c7b9556ca0eab
Pace Willisson <pace@alum.mit.edu>
2020-03-25 20:10:44 -0400

    cleanup

--- bt.c
+++ bt.c
@@ -17,11 +17,11 @@ int vflag;
 
 #define ATT_CID 4
 
+#define ATT_OP_FIND_INFO_REQ		0x04
+#define ATT_OP_FIND_INFO_RESP		0x05
 #define ATT_OP_WRITE_REQ		0x12
 
-enum {
-	ATT_OP_TYPE_REQ = 0
-};
+#define ATT_FIND_INFO_RESP_FMT_16BIT		0x01
 
 void
 usage (void)
@@ -46,13 +46,34 @@ get_secs (void)
 }
 
 int
-connect_bluetooth (bdaddr_t *dest_addr)
+connect_bluetooth (void)
 {
 	struct sockaddr_l2 src_addr;
 	struct sockaddr_l2 dst_addr;
+	char dst_addr_str[100];
+	char *fname;
+	int n;
 
+	FILE *inf = NULL;
 	int sock = -1;
 	
+	fname = ".bluetooth_addr";
+	if ((inf = fopen (fname, "r")) == NULL) {
+		fprintf (stderr, "can't open %s - maybe run ./pair?\n", fname);
+		goto bad;
+	}
+	if (fgets (dst_addr_str, sizeof dst_addr_str, inf) == NULL) {
+		fprintf (stderr, "can't read %s\n", fname);
+		fclose (inf);
+		goto bad;
+	}
+	fclose (inf);
+	inf = NULL;
+
+	n = strlen (dst_addr_str);
+	while (n > 0 && isspace (dst_addr_str[n-1]))
+		dst_addr_str[--n] = 0;
+
 	if ((sock = socket (PF_BLUETOOTH, SOCK_SEQPACKET, BTPROTO_L2CAP)) < 0) {
 		perror ("socket bt");
 		goto bad;
@@ -61,10 +82,8 @@ connect_bluetooth (bdaddr_t *dest_addr)
 	memset (&src_addr, 0, sizeof src_addr);
 	src_addr.l2_family = AF_BLUETOOTH;
 	memcpy (&src_addr.l2_bdaddr, BDADDR_ANY, sizeof src_addr.l2_bdaddr);
-	
 	src_addr.l2_cid = htobs (ATT_CID);
 	src_addr.l2_bdaddr_type = BDADDR_BREDR;
-
 	if (bind (sock, (struct sockaddr *)&src_addr, sizeof src_addr) < 0) {
 		perror ("bind");
 		goto bad;
@@ -87,7 +106,11 @@ connect_bluetooth (bdaddr_t *dest_addr)
 
 	memset (&dst_addr, 0, sizeof dst_addr);
 	dst_addr.l2_family = AF_BLUETOOTH;
-	dst_addr.l2_bdaddr = *dest_addr;
+	if (str2ba(dst_addr_str, &dst_addr.l2_bdaddr) < 0) {
+		fprintf (stderr, "can't parse address %s from %s\n",
+			 dst_addr_str, fname);
+		return (-1);
+	}
 	dst_addr.l2_cid = htobs (ATT_CID);
 	dst_addr.l2_bdaddr_type = BDADDR_LE_RANDOM;
 
@@ -132,43 +155,225 @@ connect_bluetooth (bdaddr_t *dest_addr)
 		goto bad;
 	}
 
+	if (fcntl (sock, F_SETFL, 0) < 0) {
+		perror ("fcntl turn off NONBLOCK");
+		exit (1);
+	}
+
 	return (sock);
 
 bad:
 	if (sock >= 0)
 		close (sock);
+	if (inf)
+		fclose (inf);
 	return (-1);
 }
 
-void
-await_readable (int sock)
-{
-	fd_set rset;
+struct handle {
+	char *name;
+	char *uuid_str;
+	unsigned char uuid_bin[16];
+	int handle;
+};
 
-	while (1) {
-		FD_ZERO (&rset);
-		FD_SET (sock, &rset);
-		if (select (sock + 1, &rset, NULL, NULL, NULL) < 0) {
-			perror ("select");
-			exit (1);
-		}
-		if (FD_ISSET (sock, &rset))
-			break;
+struct handle handles[] = {
+	{ "tx", "6E400002-B5A3-F393-E0A9-E50E24DCCA9E" },
+	{ "rx", "6E400003-B5A3-F393-E0A9-E50E24DCCA9E" },
+};
+
+int tx_handle;
+int rx_handle;
+
+int
+parse_uuid (char *str, unsigned char *bin)
+{
+	int i;
+	char *p;
+	int val;
+	
+	p = str;
+	for (i = 0; i < 16; i++) {
+		if (sscanf (p, "%2x", &val) != 1)
+			return (-1);
+		bin[i] = val;
+		p += 2;
+		if (*p == '-')
+			p++;
+	}
+	return (0);
+}
+
+void
+print_uuid (uint8_t *uuid)
+{
+	int i;
+	
+	for (i = 0; i < 16; i++) {
+		if (i == 4 || i == 6 || i == 8 || i == 10)
+			printf ("-");
+		printf ("%02x", uuid[i]);
 	}
 }
 
+uint8_t base_uuid[16] = {	
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x00,
+	0x80, 0x00, 0x00, 0x80, 0x5F, 0x9B, 0x34, 0xFB
+};
 
+int
+process_resp (unsigned char *pdu, int togo)
+{
+	uint8_t format;
+	int elen;
+	int count;
+	int elt;
+	uint8_t uuid[16];
+	int handle;
+	int h;
+	struct handle *hp;
+	
+	if (togo == 0)
+		return (-1);
+
+	format = *pdu++;
+	togo--;
+	
+	elen = 2;
+	if (format == ATT_FIND_INFO_RESP_FMT_16BIT) {
+		elen += 2;
+	} else {
+		elen += 16;
+	}
+
+	count = togo / elen;
+	handle = 0;
+	
+	for (elt = 0; elt < count; elt++) {
+		uint8_t valbuf[100];
+
+		if (togo < elen) {
+			printf ("runt\n");
+			break;
+		}
+		
+		memcpy (valbuf, pdu, elen);
+
+		handle = valbuf[0] | (valbuf[1] << 8);
+		
+		if (format == ATT_FIND_INFO_RESP_FMT_16BIT) {
+			memcpy (uuid, base_uuid, sizeof uuid);
+			uuid[2] = valbuf[3];
+			uuid[3] = valbuf[2];
+		} else {
+			int i;
+			for (i = 0; i < 16; i++)
+				uuid[15 - i] = valbuf[2 + i];
+		}
+
+		if (0) {
+			printf ("0x%x ", handle);
+			print_uuid (uuid);
+			printf ("\n");
+		}
+
+		for (h = 0; h < sizeof handles / sizeof handles[0]; h++) {
+			hp = &handles[h];
+			if (memcmp (hp->uuid_bin, uuid, 16) == 0)
+				hp->handle = handle;
+		}
+
+		pdu += elen;
+		togo -= elen;
+	}
+
+	return (handle);
+}
+
+int
+read_handles (int sock)
+{
+	int start_handle, end_handle;
+	struct handle *hp;
+	int h;
+	unsigned char pdu_buf[100], *pdu;
+	int used;
+	int togo;
+	int opcode;
+	int last_handle;
+	
+	for (h = 0; h < sizeof handles / sizeof handles[0]; h++) {
+		hp = &handles[h];
+		if (parse_uuid (hp->uuid_str, hp->uuid_bin) < 0) {
+			fprintf (stderr, "invalid uuid %s\n", 
+				 hp->uuid_str);
+			return (-1);
+		}
+	}
+
+	start_handle = 1;
+	end_handle = 0xffff;
+
+	while (start_handle < end_handle) {
+		pdu = pdu_buf;
+		*pdu++ = ATT_OP_FIND_INFO_REQ;
+		*pdu++ = start_handle;
+		*pdu++ = start_handle >> 8;
+		*pdu++ = end_handle;
+		*pdu++ = end_handle >> 8;
+		used = pdu - pdu_buf;
+
+		if (write (sock, pdu_buf, used) < 0) {
+			perror ("write");
+			exit (1);
+		}
+
+		if ((togo = read (sock, pdu_buf, sizeof pdu_buf)) < 0) {
+			perror ("read");
+			exit (1);
+		}
+		if (togo == 0)
+			break;
+
+		pdu = pdu_buf;
+
+		opcode = *pdu++;
+		togo--;
+
+		if (opcode != ATT_OP_FIND_INFO_RESP)
+			break;
+
+		last_handle = process_resp (pdu, togo);
+
+		if (last_handle < start_handle) {
+			printf ("didn't make proress\n");
+			break;
+		}
+
+		start_handle = last_handle + 1;
+	}
+
+	for (h = 0; h < sizeof handles / sizeof handles[0]; h++) {
+		hp = &handles[h];
+		if (hp->handle == 0) {
+			fprintf (stderr, "can't find handle for %s\n",
+				 hp->uuid_str);
+			return (-1);
+		}
+		if (strcmp (hp->name, "tx") == 0)
+			tx_handle = hp->handle;
+		else if (strcmp (hp->name, "rx") == 0)
+			rx_handle = hp->handle;
+	}
+
+	return (0);
+}
 
 int
 main (int argc, char **argv)
 {
 	int c;
 	int sock;
-	char *fname;
-	FILE *inf;
-	char addrstr[100];
-	bdaddr_t bdaddr;
-	int n;
 
 	while ((c = getopt (argc, argv, "")) != EOF) {
 		switch (c) {
@@ -177,30 +382,13 @@ main (int argc, char **argv)
 		}
 	}
 
-	fname = ".bluetooth_addr";
-	if ((inf = fopen (fname, "r")) == NULL) {
-		fprintf (stderr, "can't open %s - maybe run ./pair?\n", fname);
+	if ((sock = connect_bluetooth ()) < 0) {
+		fprintf (stderr, "can't connect\n");
 		exit (1);
 	}
-	if (fgets (addrstr, sizeof addrstr, inf) == NULL) {
-		fprintf (stderr, "can't read %s\n", fname);
-		exit (1);
-	}
-	n = strlen (addrstr);
-	while (n > 0 && isspace (addrstr[n-1]))
-		addrstr[--n] = 0;
 	
-	if (str2ba(addrstr, &bdaddr) < 0) {
-		fprintf (stderr, "can't parse address %s from %s\n",
-			 addrstr, fname);
-		exit (1);
-	}
-
-	if ((sock = connect_bluetooth (&bdaddr)) < 0)
-		exit (1);
-	
-	if (fcntl (sock, F_SETFL, 0) < 0) {
-		perror ("fcntl turn off NONBLOCK");
+	if (read_handles (sock) < 0) {
+		fprintf (stderr, "can't find handles\n");
 		exit (1);
 	}
 
