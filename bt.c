7947e78d5a58ad0a09da1d8ef99901944906837e
Pace Willisson <pace@alum.mit.edu>
2020-03-27 12:06:54 -0400

    handles

--- bt.c
+++ bt.c
@@ -13,6 +13,20 @@
 #include <bluetooth/hci.h>
 #include <bluetooth/hci_lib.h>
 
+#define ATT_CID 4
+
+#define ATT_OP_FIND_INFO_REQ		0x04
+#define ATT_OP_FIND_INFO_RESP		0x05
+#define ATT_OP_READ_BY_GROUP_REQ	0x10
+#define ATT_OP_READ_BY_GROUP_RESP	0x11
+#define ATT_OP_WRITE_REQ		0x12
+
+#define ATT_FIND_INFO_RESP_FMT_16BIT		0x01
+
+
+#define GATT_PRIM_SVC_UUID		0x2800
+
+
 void do_xmit (int sock, char *msg);
 
 void
@@ -46,14 +60,6 @@ dump (void *buf, int n)
 }
 
 int vflag;
-
-#define ATT_CID 4
-
-#define ATT_OP_FIND_INFO_REQ		0x04
-#define ATT_OP_FIND_INFO_RESP		0x05
-#define ATT_OP_WRITE_REQ		0x12
-
-#define ATT_FIND_INFO_RESP_FMT_16BIT		0x01
 
 void
 usage (void)
@@ -204,16 +210,14 @@ struct handle {
 	int handle;
 };
 
-struct handle handles[] = {
-	{ "tx", "6E400002-B5A3-F393-E0A9-E50E24DCCA9E" },
-	{ "rx", "6E400003-B5A3-F393-E0A9-E50E24DCCA9E" },
-};
+#define TX_HANDLE_UUID "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
+#define RX_HANDLE_UUID "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 
 int tx_handle;
 int rx_handle;
 
 int
-parse_uuid (char *str, unsigned char *bin)
+uuid_to_bin (char *str, unsigned char *bin)
 {
 	int i;
 	char *p;
@@ -243,79 +247,21 @@ print_uuid (uint8_t *uuid)
 	}
 }
 
-uint8_t base_uuid[16] = {	
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x00,
-	0x80, 0x00, 0x00, 0x80, 0x5F, 0x9B, 0x34, 0xFB
-};
-
-int
-process_resp (unsigned char *pdu, int togo)
+void
+uuid_to_str (unsigned char bin[16], char *str)
 {
-	uint8_t format;
-	int elen;
-	int count;
-	int elt;
-	uint8_t uuid[16];
-	int handle;
-	int h;
-	struct handle *hp;
-	
-	if (togo == 0)
-		return (-1);
+	int i;
+	char *p;
 
-	format = *pdu++;
-	togo--;
-	
-	elen = 2;
-	if (format == ATT_FIND_INFO_RESP_FMT_16BIT) {
-		elen += 2;
-	} else {
-		elen += 16;
+	p = str;
+	for (i = 0; i < 16; i++) {
+		if (i == 4 || i == 6 || i == 8 || i == 10)
+			*p++ = '-';
+		p += sprintf (p, "%02x", bin[i]);
 	}
-
-	count = togo / elen;
-	handle = 0;
-	
-	for (elt = 0; elt < count; elt++) {
-		uint8_t valbuf[100];
-
-		if (togo < elen) {
-			printf ("runt\n");
-			break;
-		}
-		
-		memcpy (valbuf, pdu, elen);
-
-		handle = valbuf[0] | (valbuf[1] << 8);
-		
-		if (format == ATT_FIND_INFO_RESP_FMT_16BIT) {
-			memcpy (uuid, base_uuid, sizeof uuid);
-			uuid[2] = valbuf[3];
-			uuid[3] = valbuf[2];
-		} else {
-			int i;
-			for (i = 0; i < 16; i++)
-				uuid[15 - i] = valbuf[2 + i];
-		}
-
-		if (0) {
-			printf ("0x%x ", handle);
-			print_uuid (uuid);
-			printf ("\n");
-		}
-
-		for (h = 0; h < sizeof handles / sizeof handles[0]; h++) {
-			hp = &handles[h];
-			if (memcmp (hp->uuid_bin, uuid, 16) == 0)
-				hp->handle = handle;
-		}
-
-		pdu += elen;
-		togo -= elen;
-	}
-
-	return (handle);
+	*p = 0;
 }
+
 
 int
 await_readable (int sock, double secs)
@@ -339,27 +285,275 @@ await_readable (int sock, double secs)
 	return (-1);
 }
 
+uint8_t base_uuid[16] = {	
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x00,
+	0x80, 0x00, 0x00, 0x80, 0x5F, 0x9B, 0x34, 0xFB
+};
+
+struct characteristic {
+	struct characteristic *next;
+	char uuid_str[50];
+	unsigned char uuid_bin[16];
+	int handle;
+};
+
+struct service {
+	struct service *next;
+	char uuid_str[50];
+	unsigned char uuid_bin[16];
+	int start_handle;
+	int end_handle;
+	struct characteristic *characteristics;
+	struct characteristic **characteristics_tail;
+
+};
+struct service *services, **services_tail = &services;
+
+void
+remember_service (unsigned char uuid_bin[16], int start_handle, int end_handle)
+{
+	struct service *sp;
+
+	sp = calloc (1, sizeof *sp);
+	sp->characteristics_tail = &sp->characteristics;
+
+	memcpy (sp->uuid_bin, uuid_bin, 16);
+	uuid_to_str (uuid_bin, sp->uuid_str);
+	sp->start_handle = start_handle;
+	sp->end_handle = end_handle;
+
+	*services_tail = sp;
+	services_tail = &sp->next;
+}
+
+struct service *
+find_service_for_handle (int handle)
+{
+	struct service *sp;
+
+	for (sp = services; sp; sp = sp->next) {
+		if (sp->start_handle <= handle && handle <= sp->end_handle)
+			return (sp);
+	}
+
+	return (NULL);
+}
+
+void
+remember_handle (int handle, unsigned char uuid_bin[16])
+{
+	struct service *sp;
+	struct characteristic *cp;
+	
+	if ((sp = find_service_for_handle (handle)) == NULL) {
+		fprintf (stderr, "can't find service for handle 0x%x\n", handle);
+		return;
+	}
+	
+	cp = calloc (1, sizeof *cp);
+	memcpy (cp->uuid_bin, uuid_bin, 16);
+	uuid_to_str (uuid_bin, cp->uuid_str);
+	cp->handle = handle;
+
+	*sp->characteristics_tail = cp;
+	sp->characteristics_tail = &cp->next;
+}	
+
+
+void
+print_services (void)
+{
+	struct service *sp;
+	
+	for (sp = services; sp; sp = sp->next) {
+		printf ("0x%04x 0x%04x %s\n", 
+			sp->start_handle, sp->end_handle, sp->uuid_str);
+	}
+}
+
+int
+lookup_handle (char *uuid_str)
+{
+	unsigned char uuid_bin[16];
+	struct service *sp;
+	struct characteristic *cp;
+
+	if (uuid_to_bin (uuid_str, uuid_bin) < 0) {
+		fprintf (stderr, "invalid uuid %s\n", uuid_str);
+		return (-1);
+	}
+
+	for (sp = services; sp; sp = sp->next) {
+		for (cp = sp->characteristics; cp; cp = cp->next) {
+			if (memcmp (cp->uuid_bin, uuid_bin, 16) == 0)
+				return (cp->handle);
+		}
+	}
+
+	return (-1);
+}
+
+void read_services (int sock);
+int read_handles (int sock);
+
+int process_read_by_group (unsigned char *pdu, int togo);
+
+int
+read_device_info (int sock)
+{
+	read_services (sock);
+	read_handles (sock);
+
+	if ((tx_handle = lookup_handle (TX_HANDLE_UUID)) < 0) {
+		fprintf (stderr, "can't find tx handle\n");
+		return (-1);
+	}
+
+	if ((rx_handle = lookup_handle (RX_HANDLE_UUID)) < 0) {
+		fprintf (stderr, "can't find rx handle\n");
+		return (-1);
+	}
+	
+	printf ("tx handle 0x%x; rx 0x%x\n", tx_handle, rx_handle);
+	return (0);
+}
+
+void
+read_services (int sock)
+{
+	int start_handle, end_handle;
+	unsigned char pdu_buf[100], *pdu;
+	int used;
+	int togo;
+	int opcode;
+	int last_handle;
+
+	start_handle = 1;
+	end_handle = 0xffff;
+
+	while (start_handle < 0xffff) {
+		pdu = pdu_buf;
+		*pdu++ = ATT_OP_READ_BY_GROUP_REQ;
+		*pdu++ = start_handle & 0xff;
+		*pdu++ = start_handle >> 8;
+		*pdu++ = end_handle & 0xff;
+		*pdu++ = end_handle >> 8;
+		*pdu++ = GATT_PRIM_SVC_UUID & 0xff;
+		*pdu++ = GATT_PRIM_SVC_UUID >> 8;
+		used = pdu - pdu_buf;
+
+		if (write (sock, pdu_buf, used) < 0) {
+			perror ("write");
+			exit (1);
+		}
+
+		if (await_readable (sock, 1.0) < 0) {
+			printf ("read timeout\n");
+			break;
+		}
+
+		if ((togo = read (sock, pdu_buf, sizeof pdu_buf)) < 0) {
+			perror ("read");
+			exit (1);
+		}
+		if (togo == 0)
+			break;
+
+		pdu = pdu_buf;
+
+		opcode = *pdu++;
+		togo--;
+
+		if (opcode != ATT_OP_READ_BY_GROUP_RESP)
+			break;
+
+		last_handle = process_read_by_group (pdu, togo);
+
+		if (last_handle < start_handle) {
+			printf ("didn't make proress\n");
+			break;
+		}
+
+		start_handle = last_handle + 1;
+	}
+
+}
+
+int
+process_read_by_group (unsigned char *pdu, int togo)
+{
+	int elen;
+	int start_handle, end_handle;
+	int count;
+	int elt;
+	unsigned char uuid[16];
+	
+	if (togo == 0)
+		return (-1);
+
+	/* dec_read_by_grp_resp */
+
+	if (vflag) {
+		printf ("grp response\n");
+		dump (pdu, togo);
+	}
+
+	elen = *pdu++;
+	togo--;
+
+	if (elen != 4+2 && elen != 4+16)
+		return (-1);
+
+	if (togo % elen != 0)
+		return (-1);
+/*
+attr handle: 0x0001, end grp handle: 0x0009 uuid: 00001800-0000-1000-8000-00805f9b34fb
+attr handle: 0x000a, end grp handle: 0x000d uuid: 00001801-0000-1000-8000-00805f9b34fb
+attr handle: 0x000e, end grp handle: 0x0011 uuid: 75c3c6d0-75e4-4223-a823-bdc65e738996
+attr handle: 0x0012, end grp handle: 0xffff uuid: 6e400001-b5a3-f393-e0a9-e50e24dcca9e
+*/
+	count = togo / elen;
+	end_handle = -1;
+	
+	for (elt = 0; elt < count; elt++) {
+		/* we know we have a complete record
+		 * since we checked that togo was a multiple of elen
+		 */ 
+		start_handle = pdu[0] | (pdu[1] << 8);
+		end_handle = pdu[2] | (pdu[3] << 8);
+		pdu += 4;
+		togo -= 4;
+		
+		if (elen == 4+2) {
+			memcpy (uuid, base_uuid, sizeof uuid);
+			uuid[2] = pdu[1];
+			uuid[3] = pdu[0];
+			pdu += 2;
+			togo -= 2;
+		} else {
+			int i;
+			for (i = 0; i < 16; i++)
+				uuid[15 - i] = pdu[i];
+		}
+			
+		remember_service (uuid, start_handle, end_handle);
+	}
+
+	return (end_handle);
+}
+
+int process_resp (unsigned char *pdu, int togo);
+
 int
 read_handles (int sock)
 {
 	int start_handle, end_handle;
-	struct handle *hp;
-	int h;
 	unsigned char pdu_buf[100], *pdu;
 	int used;
 	int togo;
 	int opcode;
 	int last_handle;
 	
-	for (h = 0; h < sizeof handles / sizeof handles[0]; h++) {
-		hp = &handles[h];
-		if (parse_uuid (hp->uuid_str, hp->uuid_bin) < 0) {
-			fprintf (stderr, "invalid uuid %s\n", 
-				 hp->uuid_str);
-			return (-1);
-		}
-	}
-
 	start_handle = 1;
 	end_handle = 0xffff;
 
@@ -407,22 +601,70 @@ read_handles (int sock)
 		start_handle = last_handle + 1;
 	}
 
-	for (h = 0; h < sizeof handles / sizeof handles[0]; h++) {
-		hp = &handles[h];
-		if (hp->handle == 0) {
-			fprintf (stderr, "can't find handle for %s\n",
-				 hp->uuid_str);
-			return (-1);
-		}
-		if (strcmp (hp->name, "tx") == 0)
-			tx_handle = hp->handle;
-		else if (strcmp (hp->name, "rx") == 0)
-			rx_handle = hp->handle;
+	return (0);
+}
+
+int
+process_resp (unsigned char *pdu, int togo)
+{
+	uint8_t format;
+	int elen;
+	int count;
+	int elt;
+	uint8_t uuid[16];
+	int handle;
+	
+	if (togo == 0)
+		return (-1);
+
+	format = *pdu++;
+	togo--;
+	
+	elen = 2;
+	if (format == ATT_FIND_INFO_RESP_FMT_16BIT) {
+		elen += 2;
+	} else {
+		elen += 16;
 	}
 
-	printf ("tx handle 0x%x; rx 0x%x\n", tx_handle, rx_handle);
+	count = togo / elen;
+	handle = 0;
+	
+	for (elt = 0; elt < count; elt++) {
+		uint8_t valbuf[100];
 
-	return (0);
+		if (togo < elen) {
+			printf ("runt\n");
+			break;
+		}
+		
+		memcpy (valbuf, pdu, elen);
+
+		handle = valbuf[0] | (valbuf[1] << 8);
+		
+		if (format == ATT_FIND_INFO_RESP_FMT_16BIT) {
+			memcpy (uuid, base_uuid, sizeof uuid);
+			uuid[2] = valbuf[3];
+			uuid[3] = valbuf[2];
+		} else {
+			int i;
+			for (i = 0; i < 16; i++)
+				uuid[15 - i] = valbuf[2 + i];
+		}
+
+		if (0) {
+			printf ("0x%x ", handle);
+			print_uuid (uuid);
+			printf ("\n");
+		}
+
+		remember_handle (handle, uuid);
+
+		pdu += elen;
+		togo -= elen;
+	}
+
+	return (handle);
 }
 
 int
@@ -443,11 +685,10 @@ main (int argc, char **argv)
 		exit (1);
 	}
 	
-	if (read_handles (sock) < 0) {
-		fprintf (stderr, "can't find handles\n");
-		exit (1);
-	}
+	read_device_info (sock);
 
+	print_services ();
+	
 	do_xmit (sock, "hello");
 
 	while (1) {
